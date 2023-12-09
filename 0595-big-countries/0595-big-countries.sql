8192ea3b8de7fc5d73bface295d7eb1186e562d1
Mark Randy Reid <code@markrandyreid.com>
2023-12-09 09:08:49 -0800

    Delete 0595-big-countries directory

--- 0595-big-countries/0595-big-countries.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-# filter countries by exclusive conditions 
-    select  name
-         ,  population
-         ,  area
-      from  World
-     where  area >= 3000000 or population >= 25000000;
-     
